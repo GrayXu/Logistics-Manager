e29a5bbe1bf4dd9783ca0ff5eaee2cc1161a0a8d
Xgl <285293728@qq.com>
2017-09-13 21:59:32 +0800

    Final version
    放弃二进制存档开发，修复重复站点解决。待完成实验报告

--- DataStructure.h
+++ DataStructure.h
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+char** setDoubleCharArray(char** out, char* in);
+
 /**
 * 货物的基本信息：*Type : 装货、卸货的货物的种类
 *                  *Volume	: 货物所占容量，（单位立方米）
@@ -8,6 +11,19 @@ typedef struct Good {
 	char downloadType[10];
 	float downVolume;
 } good;
+
+char** readGood(char* in) {
+	char** strArray = (char**)malloc(sizeof(char*) * 4);
+	strArray[0] = (char*)malloc(sizeof(char) * 10);
+	strArray[1] = (char*)malloc(sizeof(char) * 10);
+	strArray[2] = (char*)malloc(sizeof(char) * 10);
+	strArray[3] = (char*)malloc(sizeof(char) * 10);
+	if (setDoubleCharArray(strArray, in) != NULL){
+        return strArray;
+	} else {
+        return NULL;
+	}
+}
 
 /**
 * 配送车辆的基本信息：	carID： 车辆牌照
@@ -26,6 +42,20 @@ typedef struct Car {
 	good * good;
 	struct Car * next;
 } car;
+
+char ** readCar(char* in) {
+	char** strArray = (char**)malloc(sizeof(char*) * 4);
+	strArray[0] = (char*)malloc(sizeof(char) * 9);
+	strArray[1] = (char*)malloc(sizeof(char) * 7);
+	strArray[2] = (char*)malloc(sizeof(char) * 9);
+	strArray[3] = (char*)malloc(sizeof(char) * 12);
+	if (setDoubleCharArray(strArray, in) != NULL){
+        return strArray;
+	} else {
+        return NULL;
+	}
+
+}
 
 /**
 * 经停站点详细信息:	routeNums: 固定配送路线数量
@@ -53,6 +83,24 @@ typedef struct Site {
 	struct Site * next;
 
 } site;
+
+char** readSite(char* in) {
+	char** strArray = (char**)malloc(sizeof(char *) * 9);
+	strArray[0] = (char*)malloc(sizeof(char) * 7);
+	strArray[1] = (char*)malloc(sizeof(char) * 11);
+	strArray[2] = (char*)malloc(sizeof(char) * 11);
+	strArray[3] = (char*)malloc(sizeof(char) * 51);
+	strArray[4] = (char*)malloc(sizeof(char) * 11);
+	strArray[5] = (char*)malloc(sizeof(char) * 11);
+	strArray[6] = (char*)malloc(sizeof(char) * 11);
+	strArray[7] = (char*)malloc(sizeof(char) * 11);
+	strArray[8] = (char*)malloc(sizeof(char) * 51);
+	if (setDoubleCharArray(strArray, in) != NULL){
+        return strArray;
+	} else {
+        return NULL;
+	}
+}
 
 /**
 * 配送路线详细信息:	routeID: 固定配送路线编号
@@ -85,3 +133,58 @@ typedef struct Route {
 	struct Route * next;
 } route;
 
+/*把数据分解为二维字符指针*/
+char** readRoute(char* in) {
+	char** strArray = (char**)malloc(sizeof(char *) * 11);
+	strArray[0] = (char*)malloc(sizeof(char) * 7);
+	strArray[1] = (char*)malloc(sizeof(char) * 21);
+	strArray[2] = (char*)malloc(sizeof(char) * 11);
+	strArray[3] = (char*)malloc(sizeof(char) * 11);
+	strArray[4] = (char*)malloc(sizeof(char) * 11);
+	strArray[5] = (char*)malloc(sizeof(char) * 11);
+	strArray[6] = (char*)malloc(sizeof(char) * 11);
+	strArray[7] = (char*)malloc(sizeof(char) * 9);
+	strArray[8] = (char*)malloc(sizeof(char) * 9);
+	strArray[9] = (char*)malloc(sizeof(char) * 12);
+	strArray[10] = (char*)malloc(sizeof(char) * 51);
+	if (setDoubleCharArray(strArray, in) != NULL){
+        return strArray;
+	} else {
+        return NULL;
+	}
+}
+
+//free stake memory( for the double char array)
+void freeDoubleCharArray(int num, char ** info) {
+	register int i;
+	for (i = 0; i < num; i++) {
+		free(info[i]);
+	}
+	free(info);
+
+}
+
+//将一维字符数组的内容切分至已经开好空间的二维字符数组中去。(仅适用于必须有内容的情况
+char** setDoubleCharArray(char** out, char* in) {
+	if (out == NULL || in[0]=='\0') {
+		return NULL;
+	}
+	register int allIndex = 0;
+	register int outIndex = 0;
+	register int inIndex = 0;
+	char read = 0;
+
+	while ((read = *(in + allIndex)), read != '\n') {
+		if (read == ';') {
+			out[outIndex][inIndex] = '\0';//end this string
+			outIndex++;//reset
+			inIndex = 0;
+		} else {
+			out[outIndex][inIndex] = read;
+			inIndex++;
+		}
+		allIndex++;
+	}
+	out[outIndex][inIndex] = '\0';//end the final string
+	return out;
+}
